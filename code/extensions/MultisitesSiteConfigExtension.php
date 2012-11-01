20993176af852bc74312303b802d2f7057df8b8e
Simon Elvery <simon@elvery.net>
2012-11-01 12:05:04 +1000

    Make SiteConfig accessible again.

---
+++ code/extensions/MultisitesSiteConfigExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Extend the SiteConfig
+ */
+class MultisitesSiteConfigExtension extends Extension {
+	
+	/**
+	 * Modify fields in the site config screen to remove fields which are now handled by Site.
+	 * 
+	 * @param FieldList $fields The SiteConfig fields
+	 */
+	public function updateCMSFields($fields) {
+		$fields->removeByName('Title');
+		$fields->removeByName('Tagline');
+		$fields->removeByName('Theme');
+	}
+}
