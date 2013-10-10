74e541af20e99a52af74984a6fbfdcf51863fa24
Marcus Nyeholt <nyeholt@gmail.com>
2013-10-10 15:46:24 +1100

    FIX Only mark the hostmap cache as valid if there's actually hosts in there, otherwise trigger another rebuild of it

--- code/Multisites.php
+++ code/Multisites.php
@@ -55,7 +55,7 @@ class Multisites {
 	 */
 	public function init() {
 		$cached = $this->cache->load(self::CACHE_KEY);
-		$valid  = $cached && isset($cached['hosts']);
+		$valid  = $cached && isset($cached['hosts']) && count($cached['hosts']);
 
 		if($valid) {
 			$this->map = $cached;
