f85066f9d4e154a1fb98e9ca84b5f4de1d496067
Marcus Nyeholt <marcus@silverstripe.com.au>
2016-11-12 16:28:58 +1100

    FIX Remove editor_css swapover in admin interface
    Temporarily removed the editor_css swap for now
    
    * As is, will trigger .redraw on the tinymce fields which simply generates a
      second instance, which in turn means that the submission could actually be
      of the 'old' content, thus losing editor changes
    * The field being set doesn't actually set the .cms or .default config of the
      ssTinyMceConfig object anyway, just the top level, so it gets lost
    
    Needs to be re-evaluated later

--- javascript/MultisitesAdmin.js
+++ javascript/MultisitesAdmin.js
@@ -4,15 +4,15 @@
 		// check if the html editor config "content_css" needs to be updated 
 		// this will happen if the user has navigated to an area of the cms 
 		// associated with a different site than the screen they came from
-		$(document).ajaxComplete(function(e, xhr, settings) {
-			var editorCSS = xhr.getResponseHeader('X-HTMLEditor_content_css');
-			if(editorCSS){
-				if(editorCSS != ssTinyMceConfig.content_css ){
-					ssTinyMceConfig.content_css = editorCSS;
-					$('textarea.htmleditor').redraw();
-				}
-			}
-		});
+//		$(document).ajaxComplete(function(e, xhr, settings) {
+//			var editorCSS = xhr.getResponseHeader('X-HTMLEditor_content_css');
+//			if(editorCSS){
+//				if(editorCSS != ssTinyMceConfig.content_css ){
+//						ssTinyMceConfig.content_css = editorCSS;
+//					$('textarea.htmleditor').redraw();
+//				}
+//			}
+//		});
 
 	});
 
